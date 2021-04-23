be7d1fefb9517415f1542055ac4bf68b49d5bab1
Marko Mihokovic <mihokovic.marko.dev@gmail.com>
2021-04-23 19:29:11 +0200

    init commit

---
+++ CleverbitRandomNumbersGame/CleverbitRandomNumbersGame/OAuth/AuthenticationStartup.cs
@@ -0,0 +1,31 @@
+using Microsoft.Owin;
+using Microsoft.Owin.Security.OAuth;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace CleverbitRandomNumbersGame.OAuth
+{
+  public class AuthenticationStartup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      app.UseCors(CorsOptions.AllowAll);
+      var myProvider = new SimpleAuthorizationServerProvider();
+      OAuthAuthorizationServerOptions options = new OAuthAuthorizationServerOptions
+      {
+        AllowInsecureHttp = true,
+        TokenEndpointPath = new PathString("/token"),
+        AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+        Provider = myProvider
+      };
+      app.UseOAuthAuthorizationServer(options);
+      app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+      HttpConfiguration config = new HttpConfiguration();
+      WebApiConfig.Register(config);
+    }
+  }
+}
---
+++ CleverbitRandomNumbersGame/CleverbitRandomNumbersGame/OAuth/SimpleAuthorizationServerProvider.cs
@@ -0,0 +1,42 @@
+using Microsoft.Owin.Security.OAuth;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CleverbitRandomNumbersGame.OAuth
+{
+  public class SimpleAuthorizationServerProvider : OAuthAuthorizationServerProvider
+  {
+    public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
+    {
+      context.Validated();
+    }
+
+    
+    public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
+    {
+      // CORS ayarlarını set ediyoruz.
+      context.OwinContext.Response.Headers.Add("Access-Control-Allow-Origin", new[] { "*" });
+
+      Service<User> Users = new Service<User>();
+      var service_Result = Users.GetAll();
+
+      // Kullanıcının access_token alabilmesi için gerekli validation işlemlerini yapıyoruz.
+      if (service_Result.Any(x => x.Username == context.UserName && x.Password == context.Password))
+      {
+        var identity = new ClaimsIdentity(context.Options.AuthenticationType);
+
+        identity.AddClaim(new Claim("sub", context.UserName));
+        identity.AddClaim(new Claim("role", "user"));
+
+        context.Validated(identity);
+      }
+      else
+      {
+        context.SetError("invalid_grant", "Provided username and password is incorrect");
+      }
+    }
+  }
