096a073b1d2b24c05214c3e2a3a326ca707f2cc9
Marko Mihokovic <mihokovic.marko.dev@gmail.com>
2021-04-26 17:22:46 +0200

    add login service

--- CleverbitRandomNumbersGame/CleverbitRandomNumbersGame/cleverbit-game/src/app/services/login.service.ts
+++ CleverbitRandomNumbersGame/CleverbitRandomNumbersGame/cleverbit-game/src/app/services/login.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
@@ -5,5 +6,23 @@ import { Injectable } from '@angular/core';
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  login(username: string, password: string) {
+    return this.http.post<any>('/api/token', { username, password })
+      .pipe(map(user => {
+        // login successful if there's a jwt token in the response
+        if (user && user.token) {
+          // store user details and jwt token in local storage to keep user logged in between page refreshes
+          localStorage.setItem('TokenInfo', JSON.stringify(user));
+        }
+
+        return user;
+      }));
+  }
+
+  logout() {
+    // remove user from local storage to log user out
+    localStorage.removeItem('TokenInfo');
+  }
 }
